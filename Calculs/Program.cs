8fcc1db4b58686d0db0078b78785b03a86cbf4bc
thomas.kichelm <thomas.kichelm@LENOVO-IDEAPAD>
2022-09-20 20:19:57 +0200

    optimisation du code en n'ecrivant qu'une fois l'affectation des nombre aleatoire

--- Calculs/Program.cs
+++ Calculs/Program.cs
@@ -31,11 +31,12 @@ namespace Calculs
                 // traitement des choix
                 if (choix != 0)
                 {
+                    val1 = rand.Next(1, 10);
+                    val2 = rand.Next(1, 10);
                     if (choix == 1)
                     {
                         // choix de l'addition
-                        val1 = rand.Next(1, 10);
-                        val2 = rand.Next(1, 10);
+
                         // saisie de la réponse
                         Console.Write(val1 + " + " + val2 + " = ");
                         reponse = int.Parse(Console.ReadLine());
@@ -52,9 +53,6 @@ namespace Calculs
                     }
                     if (choix == 2) { 
                     {
-                        // choix de la multiplication
-                        val1 = rand.Next(1, 10);
-                        val2 = rand.Next(1, 10);
                         // saisie de la réponse
                         Console.Write(val1 + " x " + val2 + " = ");
                         reponse = int.Parse(Console.ReadLine());
